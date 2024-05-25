7a0a2222d829990ae7c600e4f92ac30b3d2a6e87
daniel-chou-rainho <choudaniel3@gmail.com>
2024-05-25 12:33:12 +0200

    created processData function

--- src/main.m
+++ src/main.m
@@ -1,18 +1,15 @@
 % Load Data
 chessData = loadData('../data/games.csv');
 
-% Group by opening_name and count the occurrences
-summaryTable = groupsummary(chessData, 'opening_name', 'IncludeMissingGroups', false);
-
-% Sort the table based on count in descending order
-sortedTable = sortrows(summaryTable, 'GroupCount', 'descend');
+% Process Data
+processedData = processData(chessData);
 
 % Create the categorical array from the sorted table to enforce the order in the plot
-categories = categorical(sortedTable.opening_name(1:10), sortedTable.opening_name(1:10), 'Ordinal', true);
+categories = categorical(processedData.opening_name(1:10), processedData.opening_name(1:10), 'Ordinal', true);
 
 % Display the most common openings in a bar chart
 fig = figure; % Create a new figure window
-bar(categories, sortedTable.GroupCount(1:10));
+bar(categories, processedData.GroupCount(1:10));
 title('Top 10 Most Common Chess Openings');
 xlabel('Opening Name');
 ylabel('Frequency');
@@ -23,5 +20,5 @@ grid on; % Enable grid for easier reading
 saveas(fig, '../results/MostCommonOpenings.png');
 
 % Save the data to CSV
-topOpeningsData = sortedTable(1:10, :);
+topOpeningsData = processedData(1:10, :);
 writetable(topOpeningsData, '../results/MostCommonOpeningsData.csv');
