a61e6d7d5faf6c9d287acf358bb47fab4c94e439
daniel-chou-rainho <choudaniel3@gmail.com>
2024-05-25 13:56:22 +0200

    documented saveResults function

--- src/saveResults.m
+++ src/saveResults.m
@@ -1,4 +1,14 @@
 function saveResults(summaryTable, filename)
+    % saveResults Function
+    % Saves the provided summary table to a CSV file.
+    % Inputs:
+    %   summaryTable - Table to be saved.
+    %   filename - String specifying the full path and file name for the CSV.
+    % Outputs:
+    %   None, but writes the table to the specified CSV file.
+    % Throws:
+    %   error - If the input table is empty, the directory does not exist, or there is a failure during file writing.
+
     if isempty(summaryTable)
         error('Input table is empty, cannot save results');
     end
