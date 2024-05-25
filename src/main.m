7ae6a8eb9f7fe3ef2ca61a771e20fabe6e40fcb9
daniel-chou-rainho <choudaniel3@gmail.com>
2024-05-25 12:49:25 +0200

    created main function

--- src/main.m
+++ src/main.m
@@ -1,19 +1,17 @@
-% Load Data
-chessData = loadData('../data/games.csv');
+function main(dataFilePath, resultsFolderPath)
+    % Load Data
+    chessData = loadData(dataFilePath);
 
-% Process Data
-processedData = processData(chessData);
+    % Process Data
+    processedData = processData(chessData);
 
-% Create the categorical array from the sorted table to enforce the order in the plot
-categories = categorical(processedData.opening_name(1:10), processedData.opening_name(1:10), 'Ordinal', true);
+    % Define paths for saving results
+    figurePath = fullfile(resultsFolderPath, 'MostCommonOpenings.png');
+    csvPath = fullfile(resultsFolderPath, 'MostCommonOpeningsData.csv');
 
-% Define paths for saving results
-resultsFolderPath = '../results';
-figurePath = fullfile(resultsFolderPath, 'MostCommonOpenings.png');
-csvPath = fullfile(resultsFolderPath, 'MostCommonOpeningsData.csv');
+    % Plot and save the figure of the top 10 openings
+    plotOpenings(processedData, figurePath);
 
-% Plot and save the figure of the top 10 openings
-plotOpenings(processedData, figurePath);
-
-% Save the processed data to a CSV file
-saveResults(processedData(1:10, :), csvPath);
+    % Save the processed data to a CSV file
+    saveResults(processedData(1:10, :), csvPath);
+end
